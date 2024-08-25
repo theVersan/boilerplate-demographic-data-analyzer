3b9a9b61dd70511e5fe81bf60c1a1c3aeabc015f
theVersan <davi.versan@sou.inteli.edu.br>
2024-08-24 21:56:09 -0300

    feat: adiciona código a ser executado

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,42 +3,47 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race']
+    race_count.value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df[df['sex'] == 'Male']['age'].mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    bachelors = df['education'] == 'Bachelors'
+    percentage_bachelors = bachelors.sum() / len(df) * 100
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    higher_education_rich = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    percentage = (df[higher_education_rich & (df['salary'] == '>50K')].shape[0] / df[higher_education_rich].shape[0]) * 100
+    percentage
+
     # What percentage of people without advanced education make more than 50K?
-
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
-
-    # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    lower_education_rich = ~higher_education_rich
+    new_percentage = (df[lower_education_rich & (df['salary'] == '>50K')].shape[0] / df[lower_education_rich].shape[0]) * 100
+    new_percentage
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
+    min_work_hours
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df['hours-per-week'] == min_work_hours]
 
-    rich_percentage = None
+    rich_percentage = round(len(num_min_workers[num_min_workers.salary == '50K']) / len(num_min_workers) * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    highest_earning_country= (df[df['salary'] == '>50k'])
+    highest_earning_country_percentage = highest_earning_country['native-country'].value_counts() / df['native-country'].value_counts() * 100
+    highest_earning_country.idmax(), highest_earning_country.max()
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    top_IN_occupation = df[(df['native-country'] == 'India') & (df['salary'] == '>50K')]
+    top_IN_occupation = top_IN_occupation['occupation'].value_counts()
+    top_IN_occupation.idmax()
 
     # DO NOT MODIFY BELOW THIS LINE
 
@@ -64,6 +69,6 @@ def calculate_demographic_data(print_data=True):
         'rich_percentage': rich_percentage,
         'highest_earning_country': highest_earning_country,
         'highest_earning_country_percentage':
-        highest_earning_country_percentage,
+        'highest_earning_country_percentage',
         'top_IN_occupation': top_IN_occupation
     }
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from unittest import main
 
 # Test your function by calling it here
 demographic_data_analyzer.calculate_demographic_data()
+demographic_data_analyzer.race_count.value_counts()
+
+
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
